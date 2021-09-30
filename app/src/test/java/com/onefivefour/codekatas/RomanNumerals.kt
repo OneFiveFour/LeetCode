f2ef27670e902108685e7ada46eb93409ce5ef73
Florian Meyer <florian.meyer@onefivefour.net>
2021-09-30 23:19:30 +0200

    add solution for roman numerals

--- app/src/test/java/com/onefivefour/codekatas/RomanNumerals.kt
+++ app/src/test/java/com/onefivefour/codekatas/RomanNumerals.kt
@@ -1,8 +1,7 @@
 package com.onefivefour.codekatas
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,13 +9,43 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class RomanNumerals {
-    
 
 
-    private fun encode(num: Int) : String {
-        
-        
-        return ""
+    fun encode(num: Int): String {
+
+        return num.toString()
+            .toCharArray() // split into single chars
+            .reversed() // reverse so that we ensure to go from ones to (ten)-thousands
+            .mapIndexed { index, digit ->
+
+                val value = Character.getNumericValue(digit)
+
+                when (index) {
+                    0 -> get(value, "I", "V", "X") // ones
+                    1 -> get(value, "X", "L", "C") // tens
+                    2 -> get(value, "C", "D", "M") // hundreds
+                    else -> getThousands(value) // thousands
+                }
+            }
+            .reversed() // reverse back to correct order of digits
+            .joinToString("") // join char array to final output
+    }
+
+
+
+    private fun get(value: Int, oneChar: String, fiveChar: String, tenChar: String): String {
+        return when {
+            value <= 3 -> oneChar.repeat(value)
+            value == 4 -> oneChar + fiveChar
+            value <= 8 -> fiveChar + oneChar.repeat(value - 5)
+            value == 9 -> oneChar + tenChar
+            else -> ""
+        }
+    }
+
+
+    private fun getThousands(thousands: Int): String {
+        return "M".repeat(thousands)
     }
 
 
